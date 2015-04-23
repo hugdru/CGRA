6d3dba36b7aeb71d147869a655fcebc463950ddc
Hugo Drumond <hugo.drumond@fe.up.pt>
2015-04-23 03:31:13 +0100

    corrigi um erro nas horas

--- tp5HugoArtur/MyClock.js
+++ tp5HugoArtur/MyClock.js
@@ -14,10 +14,9 @@ function MyClock(scene, clockAppearance, hourHandAppearance, minuteHandAppearanc
     this.hourHand = new MyClockHand(this.scene, 0.050, 0.50, 90);
     this.minuteHand = new MyClockHand(this.scene, 0.035, 0.80, 180);
     this.secondHand = new MyClockHand(this.scene, 0.010, 0.85, 210);
-    this.degToRad = Math.PI / 180;
 
-    this.secondsAndMinutesToDegree = 180 / 30;
-    this.hoursToDegree = 180 / 6;
+    this.clockDegreeStepSecondsMinutes = 6;
+    this.clockDegreeStepHour = 30;
 }
 
 MyClock.prototype = Object.create(CGFobject.prototype);
@@ -29,14 +28,14 @@ MyClock.prototype.update = function(currTime) {
     } else {
         var diffTime = currTime - this.setStartingTime;
 
-        var seconds = diffTime / 1000;
-        this.secondHand.setAngle(Math.round(seconds) * this.secondsAndMinutesToDegree);
+        var seconds = diffTime / 1000 * 100;
+        this.secondHand.setAngle(Math.round(seconds) * this.clockDegreeStepSecondsMinutes);
 
         var minutes = seconds / 60;
-        this.minuteHand.setAngle(minutes * this.secondsAndMinutesToDegree);
+        this.minuteHand.setAngle(minutes * this.clockDegreeStepSecondsMinutes);
 
         var hours = minutes / 60;
-        this.hourHand.setAngle(hours * this.hoursToDegree);
+        this.hourHand.setAngle(hours * this.clockDegreeStepHour);
 
     }
 };

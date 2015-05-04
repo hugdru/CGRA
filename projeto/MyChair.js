d29dcfeb9d541d73d17073d2842d0cb19bc82147
Hugo Drumond <hugo.drumond@fe.up.pt>
2015-05-04 15:43:15 +0100

    melhorei a mesa e a cadeira

--- projeto/MyChair.js
+++ projeto/MyChair.js
@@ -17,6 +17,19 @@ function MyChair(scene,
     this.cubeEncosto = new MyCube(this.scene, nrDivsEncosto, minSEncosto, maxSEncosto, minTEncosto, maxTEncosto);
     this.cubeTampo = new MyCube(this.scene, nrDivsTampo, minSTampo, maxSTampo, minTTampo, maxTTampo);
     this.cubePernas = new MyCube(this.scene, nrDivsPernas, minSPernas, maxSPernas, minTPernas, maxTPernas);
+
+    this.encostoScale = {x: 2, y: 2, z: 0.125};
+    this.tampoScale = {x: 2, y: 0.25, z: 2};
+    this.pernasScale = {x: 0.2, y: 2.5, z: 0.2};
+
+    this.halfEncosto = {};
+    this.halfTampo = {};
+    this.halfPernas = {};
+    for (var property in this.encostoScale) {
+        this.halfEncosto[property] = this.encostoScale[property] / 2;
+        this.halfTampo[property] = this.tampoScale[property] / 2;
+        this.halfPernas[property] = this.pernasScale[property] / 2;
+    }
 }
 
 MyChair.prototype = Object.create(CGFobject.prototype);
@@ -25,31 +38,36 @@ MyChair.prototype.constructor = MyChair;
 MyChair.prototype.display = function() {
 
     this.scene.pushMatrix();
-        this.scene.translate(0, 1.25, 0);
+        this.scene.translate(0, this.halfPernas.y, 0);
 
         // Criar encosto
         if (typeof this.materialEncosto !== 'undefined') this.materialEncosto.apply();
         this.scene.pushMatrix();
-            this.scene.translate(0, 2.375 + 0.125, 0.9375);
-            this.scene.scale(2, 2, 0.125);
+            this.scene.translate(
+                0,
+                this.halfEncosto.y + this.tampoScale.y + this.halfPernas.y,
+                this.halfTampo.z - this.halfEncosto.z);
+            this.scene.scale(this.encostoScale.x, this.encostoScale.y, this.encostoScale.z);
             this.cubeEncosto.display();
         this.scene.popMatrix();
 
         // Criar tampo
         if (typeof this.materialTampo !== 'undefined') this.materialTampo.apply();
         this.scene.pushMatrix();
-            this.scene.translate(0, 1.375, 0);
-            this.scene.scale(2, 0.25, 2);
+            this.scene.translate(0, this.halfTampo.y + this.halfPernas.y, 0);
+            this.scene.scale(this.tampoScale.x, this.tampoScale.y, this.tampoScale.z);
             this.cubeTampo.display();
         this.scene.popMatrix();
 
         // Criar pernas
         if (typeof this.materialPernas !== 'undefined') this.materialPernas.apply();
+        var pernasLocationX = this.halfTampo.x - this.halfPernas.x;
+        var pernasLocationZ = this.halfTampo.z - this.halfPernas.z;
         for (var i = 0; i < 4; ++i) {
             this.scene.pushMatrix();
                 this.scene.rotate((Math.PI / 2) * i, 0, -1, 0);
-                this.scene.translate(0.9, 0, 0.9);
-                this.scene.scale(0.2, 2.5, 0.2);
+                this.scene.translate(pernasLocationX, 0, pernasLocationZ);
+                this.scene.scale(this.pernasScale.x, this.pernasScale.y, this.pernasScale.z);
                 this.cubePernas.display();
             this.scene.popMatrix();
         }
--- projeto/MyTable.js
+++ projeto/MyTable.js
@@ -15,9 +15,15 @@ function MyTable(scene, topAppearance, legsAppearance,
     this.topAppearance = topAppearance;
     this.legsAppearance = legsAppearance;
 
-    this.tableScale = {x:5, y:0.3, z:3};
-    this.legScale = {x:0.3, y:3.5, z:0.3};
+    this.tableScale = {x: 5, y: 0.3, z: 3};
+    this.legScale = {x: 0.3, y: 3.5, z: 0.3};
 
+    this.halfTable = {};
+    this.halfLeg = {};
+    for (var property in this.tableScale) {
+        this.halfTable[property] = this.tableScale[property] / 2;
+        this.halfLeg[property] = this.legScale[property] / 2;
+    }
 }
 
 MyTable.prototype = Object.create(CGFobject.prototype);
@@ -25,40 +31,36 @@ MyTable.prototype.constructor = MyTable;
 
 MyTable.prototype.display = function() {
 
+
     // Criar tampo
     this.scene.pushMatrix();
         if (typeof this.topAppearance !== 'undefined') this.topAppearance.apply();
-        this.scene.translate(0, this.legScale.y / 2, 0);
+        this.scene.translate(0, this.halfLeg.y, 0);
         this.scene.pushMatrix();
-            this.scene.translate(0, this.legScale.y / 2 + this.tableScale.y / 2, 0);
+            this.scene.translate(0, this.halfLeg.y + this.halfTable.y, 0);
             this.scene.scale(this.tableScale.x, this.tableScale.y, this.tableScale.z);
             this.topCube.display();
         this.scene.popMatrix();
 
-        var halfTableX = this.tableScale.x / 2;
-        var halfTableZ = this.tableScale.z / 2;
-        var halfLegX = this.legScale.x / 2;
-        var halfLegZ = this.legScale.z / 2;
-
         // Criar pernas
         if (typeof this.legsAppearance !== 'undefined') this.legsAppearance.apply();
         this.scene.pushMatrix();
-            this.scene.translate(halfTableX - halfLegX, 0, halfTableZ - halfLegZ);
+            this.scene.translate(this.halfTable.x - this.halfLeg.x, 0, this.halfTable.z - this.halfLeg.z);
             this.scene.scale(this.legScale.x, this.legScale.y, this.legScale.z);
             this.legsCube.display();
         this.scene.popMatrix();
         this.scene.pushMatrix();
-            this.scene.translate(halfTableX - halfLegX, 0, -halfTableZ + halfLegZ);
+            this.scene.translate(this.halfTable.x - this.halfLeg.x, 0, -this.halfTable.z + this.halfLeg.z);
             this.scene.scale(this.legScale.x, this.legScale.y, this.legScale.z);
             this.legsCube.display();
         this.scene.popMatrix();
         this.scene.pushMatrix();
-            this.scene.translate(-halfTableX + halfLegX, 0, -halfTableZ + halfLegZ);
+            this.scene.translate(-this.halfTable.x + this.halfLeg.x, 0, -this.halfTable.z + this.halfLeg.z);
             this.scene.scale(this.legScale.x, this.legScale.y, this.legScale.z);
             this.legsCube.display();
         this.scene.popMatrix();
         this.scene.pushMatrix();
-            this.scene.translate(-halfTableX + halfLegX, 0, halfTableZ - halfLegZ);
+            this.scene.translate(-this.halfTable.x + this.halfLeg.x, 0, this.halfTable.z - this.halfLeg.z);
             this.scene.scale(this.legScale.x, this.legScale.y, this.legScale.z);
             this.legsCube.display();
         this.scene.popMatrix();
